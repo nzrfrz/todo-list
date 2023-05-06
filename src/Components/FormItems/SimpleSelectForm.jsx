395e88d77c994e33f0768af6b4c85620e97c587a
nzr.frz <nzr.frz@gmail.com>
2023-05-06 17:20:40 +0700

    #5

--- src/Components/FormItems/SimpleSelectForm.jsx
+++ src/Components/FormItems/SimpleSelectForm.jsx
@@ -25,9 +25,8 @@ export const SimpleSelectForm = ({name, label, optionSelect, requiredMark = true
             ]}
         >
             <Select
-                showSearch
-                allowClear
                 size="large"
+                data-cy="modal-add-priority-item"
                 placeholder={`Select ${label}`}
                 style={{
                     width: "50%"
@@ -36,7 +35,7 @@ export const SimpleSelectForm = ({name, label, optionSelect, requiredMark = true
                 {
                     optionSelect.map((data, index) => 
                         <Select.Option data-cy="modal-add-priority-item" key={index} value={data.value} >
-                            <div data-cy="modal-add-priority-item" style={{ display: "flex", gap: "12px" }}>
+                            <div style={{ display: "flex", gap: "12px" }}>
                                 <PriorityBadge 
                                     priority={data.value}
                                 />
