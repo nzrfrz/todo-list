1c97903fb9a75bb2eb679cf2d68467f101d83ad3
nzr.frz <nzr.frz@gmail.com>
2023-05-06 16:20:19 +0700

    add data to dropdown priority

--- src/Components/FormItems/SimpleSelectForm.jsx
+++ src/Components/FormItems/SimpleSelectForm.jsx
@@ -36,7 +36,7 @@ export const SimpleSelectForm = ({name, label, optionSelect, requiredMark = true
             >
                 {
                     optionSelect.map((data, index) => 
-                        <Select.Option key={index} value={data.value} >
+                        <Select.Option data-cy="modal-add-priority-item" key={index} value={data.value} >
                             <div style={{ display: "flex", gap: "12px" }}>
                                 <PriorityBadge 
                                     priority={data.value}
